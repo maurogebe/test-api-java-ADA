deb6aab67e1e6f95f7378a960bceb5f278a2d430
jorgeHenry <jorgerojas117@hotmail.com>
2024-07-25 09:22:24 -0500

    [feature][QP-12][funcionalidad leer imagen y pdf]

---
+++ src/main/java/com/example/demo/application/usecases/TesseractConfigUseCase.java
@@ -0,0 +1,26 @@
+package com.example.demo.application.usecases;
+
+import net.sourceforge.tess4j.Tesseract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class TesseractConfigUseCase {
+
+    @Autowired
+    Tesseract tesseract;
+
+    public String recognizeText(InputStream inputStream) throws IOException {
+        BufferedImage image = ImageIO.read(inputStream);
+        try {
+            return tesseract.doOCR(image);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "failed";
+    }
+}
---
+++ src/main/java/com/example/demo/infrastructure/controllers/TesseractOcrController.java
@@ -0,0 +1,23 @@
+package com.example.demo.infrastructure.controllers;
+
+import com.example.demo.application.usecases.TesseractConfigUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ocr")
+public class TesseractOcrController {
+
+    @Autowired
+    private TesseractConfigUseCase tesseractConfigUseCase;
+    @PostMapping
+    public String recognizeText(@RequestParam (name = "img") MultipartFile img) throws IOException {
+        return tesseractConfigUseCase.recognizeText(img.getInputStream ());
+    }
+}
